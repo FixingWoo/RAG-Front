571ccdabfacd944f5bc4fd244995ee6b26042e84
FixingWoo <fixingwoo@gmail.com>
2025-02-28 15:59:36 +0900

    feat: Placeholder 컴포넌트

---
+++ src/components/Placeholder/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './style.module.scss';
+
+interface IProps {
+  text: string;
+}
+
+const Placeholder: React.FC<IProps> = ({ text }) => {
+  return <p className={styles.placeholder} data-placeholder={text}></p>;
+};
+
+Placeholder.displayName = 'Placeholder';
+
+export default Placeholder;
