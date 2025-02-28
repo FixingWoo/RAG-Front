81b3bf6e8652c46b1fd5b2c0c638ae19b52d5de9
FixingWoo <fixingwoo@gmail.com>
2025-02-28 10:48:47 +0900

    chore: 의존성 설치 및 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import React from 'react';
+import styles from './style.module.scss';
+
+import VisuallyHidden from '@/components/VisuallyHidden';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <button>테스트</button>
+      <VisuallyHidden>테스트</VisuallyHidden>
+    </>
+  );
 }
