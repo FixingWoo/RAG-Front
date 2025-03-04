779b800608c7e4ac286add37821aacef28faa792
FixingWoo <fixingwoo@gmail.com>
2025-03-04 16:28:45 +0900

    test: 테스트 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,10 @@ import ChatContent from '@/components/Chat/ChatContent';
 import ChatForm from '@/components/Chat/ChatForm';
 
 import { useChatStore } from '@/stores';
-import { test } from '@/apis';
 
 export default function Home() {
   const containerRef = useRef<HTMLDivElement>(null);
   const { chats } = useChatStore();
-
-  useEffect(() => {
-    async function fetch() {
-      const result = await test();
-
-      console.log(result);
-    }
-
-    fetch();
-  }, []);
 
   useEffect(() => {
     document.body.scrollTop = document.body.scrollHeight;
