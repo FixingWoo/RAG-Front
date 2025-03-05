7df7801766ae02bff459fdca0ade61bb7c58fa4a
FixingWoo <fixingwoo@gmail.com>
2025-03-05 14:27:04 +0900

    feat: ChatHeader 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useRef } from 'react';
 import styles from './style.module.scss';
 
+import ChatHeader from '@/components/Chat/ChatHeader';
 import ChatContent from '@/components/Chat/ChatContent';
 import ChatForm from '@/components/Chat/ChatForm';
 
@@ -18,15 +19,19 @@ export default function Home() {
 
   return (
     <>
-      <main className={styles.main} ref={containerRef}>
-        <ChatContent />
-        <div className={styles.formWrapper}>
-          <ChatForm />
-          <div className={styles.notice}>
-            챗봇은 실수를 할 수 있습니다. 중요한 정보를 확인하세요.
+      <div className={styles.container} ref={containerRef}>
+        <ChatHeader />
+
+        <div className={styles.main}>
+          <ChatContent />
+          <div className={styles.formWrapper}>
+            <ChatForm />
+            <div className={styles.notice}>
+              챗봇은 실수를 할 수 있습니다. 중요한 정보를 확인하세요.
+            </div>
           </div>
         </div>
-      </main>
+      </div>
     </>
   );
 }
