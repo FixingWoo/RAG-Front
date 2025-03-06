a0a2243e32d609920234daba193a9da68634a65b
FixingWoo <fixingwoo@gmail.com>
2025-03-06 16:56:23 +0900

    fix: 로딩과 답변 높이 차이

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import styles from './style.module.scss';
 const Loading: React.FC = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.loading}></div>
+    </div>
+  );
 };
 
 export default Loading;
